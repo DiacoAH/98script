f363f42db89820e39607e28b09ccb78f032356da
DiacoAH <Diaco.ah@outlook.com>
2023-01-06 16:11:40 +0330

    feat-develop use added indicators
    use tsi and cci indicators in main loop for more signal confirm
    more send message for debug
    change message text for better user experience

--- main.py
+++ main.py
@@ -1,6 +1,8 @@
 import ssl_channel , Data
 from datetime import datetime , timedelta
-import bot.bot as bt
+import bot as bt
+import tsi
+import cci
 import time
 from time import sleep
 
@@ -13,6 +15,8 @@ data = Data.Collector()
 data.getNewData(interval)
 ssl1 = ssl_channel.Ssl()
 ssl2 = ssl_channel.Ssl()
+tsi = tsi.tsi()
+cci = cci.cci()
 
 with open(filepath, 'r') as file:
             
@@ -31,16 +35,27 @@ while True :
         data.getNewData(interval)
         df = ssl1.apply(interval, 50,50)
         df2 = ssl2.apply(interval, 100,100)
+        df3 = tsi.apply(interval, 25, 13, 13)
+        df4 = cci.apply(interval, 40)
         lts = int(df['time'][df.index[-1]])
         next_ts = datetime.timestamp(datetime.fromtimestamp(lts) + timedelta(minutes=interval*2 , seconds=10))
         print(f'next_ts {next_ts}')
         if df['sig'][df.index[-1]] != df['sig'][df.index[-2]]:
             
-            bt.send(f"🔵Ssl 50🔵 \n Cci \n Tma")
+            bt.send(f"🔵Ssl 50🔵")
+            if (df['sig'][df.index[-1]] == df4['sig'][df.index[-1]]) :
+                
+                bt.send(f"🔵Ssl 50🔵  cci ✅")
+
             print(f"ssl 50 50 \n {interval} min kandel")
+            
         if df2['sig'][df2.index[-1]] != df2['sig'][df2.index[-2]]:
             
-            bt.send(f"🟡Ssl 100🟡 \n Tsi \n Tma")
+            bt.send(f"🟡Ssl 100🟡")
+            if (df['sig'][df.index[-1]] == df3['sig'][df.index[-1]]) :
+                
+                bt.send(f"🟡Ssl 100🟡 \n Tsi ✅")
+                
             print(f"ssl 100 100 \n {interval} min kandel")
     else:
         # bt.send('waiting')
